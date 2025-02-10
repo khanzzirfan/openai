232516b06653f682d96ae55891b1b2fe6d9d7408
Irfan Khan <irfan@infopower.co.nz>
2025-02-10 15:35:39 +1100

    fix: updated openai

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,31 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 import { getInput, debug, setOutput, setFailed } from "@actions/core";
 import { chatSchema, completionsSchema, modeSchema } from "./schemas";
 
 export async function run() {
   try {
     const apiKey = getInput("openai-api-key");
-    const configuration = new Configuration({
-      apiKey,
-    });
-    const openai = new OpenAIApi(configuration);
+    const client = new OpenAI({ apiKey });
 
     // debug mode
     const inputMode = getInput("openai-mode");
     debug(`Running with mode: ${inputMode}`);
 
-    const modeSafeParseResult = modeSchema.safeParse(getInput("openai-mode"));
-
+    const modeSafeParseResult = modeSchema.safeParse(inputMode);
     if (!modeSafeParseResult.success) {
       setFailed(`Invalid mode: ${modeSafeParseResult.error}`);
       return;
     }
 
     const mode = modeSafeParseResult.data;
-
     debug(`Running in ${mode} mode`);
-    debug(`With params: ${JSON.parse(getInput("openai-params"))}`);
+
+    const params = JSON.parse(getInput("openai-params"));
+    debug(`With params: ${JSON.stringify(params)}`);
 
     switch (mode) {
       case "chat": {
-        const paramsSafeParseResult = chatSchema.safeParse(
-          JSON.parse(getInput("openai-params"))
-        );
-
+        const paramsSafeParseResult = chatSchema.safeParse(params);
         if (!paramsSafeParseResult.success) {
           setFailed(
             `Invalid params for chat mode: ${paramsSafeParseResult.error}`
@@ -39,33 +33,29 @@ export async function run() {
           return;
         }
 
-        const response = await openai.createChatCompletion(
+        const response = await client.chat.completions.create(
           paramsSafeParseResult.data
         );
-        const completion = response.data.choices[0].message?.content ?? "";
+        const completion = response.choices[0].message?.content ?? "";
 
         setOutput("completion", completion.trim());
         break;
       }
 
       case "completion": {
-        const paramsSafeParseResult = completionsSchema.safeParse(
-          JSON.parse(getInput("openai-params"))
-        );
-
+        const paramsSafeParseResult = completionsSchema.safeParse(params);
         if (!paramsSafeParseResult.success) {
           setFailed(
-            `Invalid params for chat mode: ${paramsSafeParseResult.error}`
+            `Invalid params for completion mode: ${paramsSafeParseResult.error}`
           );
           return;
         }
 
-        const response = await openai.createCompletion(
+        const response = await client.completions.create(
           paramsSafeParseResult.data
         );
-        const completion = response.data.choices[0].text ?? "";
+        const completion = response.choices[0].text ?? "";
 
-        // The output of this action is the text from OpenAI trimmed and escaped
         setOutput("completion", completion.trim());
         break;
       }
