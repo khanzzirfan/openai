5c54c3e0e3ee863a314e86affa8227bfaeb048fd
Irfan Khan <irfan@infopower.co.nz>
2025-02-10 16:03:01 +1100

    fix: updated chat completion

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
 import OpenAI from "openai";
 import { getInput, debug, setOutput, setFailed } from "@actions/core";
-import { chatSchema, modeSchema } from "./schemas";
+import { chatSchema } from "./schemas";
 
 export async function run() {
   try {
     const apiKey = getInput("openai-api-key");
     const client = new OpenAI({ apiKey });
 
-    // debug mode
-    const inputMode = getInput("openai-mode");
-    debug(`Running with mode: ${inputMode}`);
-
-    const modeSafeParseResult = modeSchema.safeParse(inputMode);
-    if (!modeSafeParseResult.success) {
-      setFailed(`Invalid mode: ${modeSafeParseResult.error}`);
-      return;
-    }
-
-    const mode = modeSafeParseResult.data;
-    debug(`Running in ${mode} mode`);
+    // Debug mode
+    debug(`Running chat completion mode`);
 
     const params = JSON.parse(getInput("openai-params"));
     debug(`With params: ${JSON.stringify(params)}`);
@@ -29,9 +19,11 @@ export async function run() {
       return;
     }
 
-    const response = await client.chat.completions.create(
-      paramsSafeParseResult.data
-    );
+    const response = await client.chat.completions.create({
+      model: "gpt-4o",
+      store: true,
+      messages: paramsSafeParseResult.data.messages,
+    });
 
     const completion = response.choices[0].message?.content ?? "";
     setOutput("completion", completion.trim());
